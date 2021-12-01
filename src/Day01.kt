5c49c4cf3242634e36dd112adb86dc06ba50ee4e
n0138988 <nolan.lichti@libertymutual.com>
2021-12-01 16:58:07 -0500

    add day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,21 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int =
+        input.map { it.toInt() }
+            .foldIndexed(0) { index: Int, count: Int, num: Int ->
+                if (index > 0 && num > input[index - 1].toInt()) count + 1 else count
+            }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int =
+        input.map {it.toInt()}
+            .foldIndexed(0) { index, count, num ->
+                if (index > 2 && num > input[index - 3].toInt()) count + 1 else count
+            }
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7) { "Expected 7, was ${part1(testInput)}" }
+    check(part2(testInput) == 5) { "Expected 5, was ${part2(testInput)}" }
 
     val input = readInput("Day01")
     println(part1(input))
